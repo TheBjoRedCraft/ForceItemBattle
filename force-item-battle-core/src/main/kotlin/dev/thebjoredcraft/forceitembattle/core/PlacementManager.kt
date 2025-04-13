5224464a2c632de1795f0c1fb36b94bed79a041a
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-13 02:37:23 +0200

    feat: enhance PlacementManager with BattlePlacement support and new methods

--- force-item-battle-core/src/main/kotlin/dev/thebjoredcraft/forceitembattle/core/PlacementManager.kt
+++ force-item-battle-core/src/main/kotlin/dev/thebjoredcraft/forceitembattle/core/PlacementManager.kt
@@ -1,7 +1,12 @@
 package dev.thebjoredcraft.forceitembattle.core
 
+import dev.thebjoredcraft.forceitembattle.api.model.BattlePlacement
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap
 import java.util.UUID
 
 interface PlacementManager {
-    fun getPlacement(uuid: UUID):
+    fun getPlacement(uuid: UUID): BattlePlacement
+    fun updatePlacement(uuid: UUID, placement: BattlePlacement)
+
+    fun getAllPlacements(): Object2ObjectMap<UUID, BattlePlacement>
 }
